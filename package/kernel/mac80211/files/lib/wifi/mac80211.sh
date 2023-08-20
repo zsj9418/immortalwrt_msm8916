e3d83d3f3fc8ed07682908bb186014d054250a1b
neighbads <neighbads@gmail.com>
2023-08-20 12:31:29 +0800

    change wifi, ip name

--- package/kernel/mac80211/files/lib/wifi/mac80211.sh
+++ package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -210,7 +210,7 @@ detect_mac80211() {
 			set wireless.default_${name}.device=${name}
 			set wireless.default_${name}.network=lan
 			set wireless.default_${name}.mode=ap
-			set wireless.default_${name}.ssid=ImmortalWrt
+			set wireless.default_${name}.ssid=ufi-$(cat /sys/class/ieee80211/${dev}/macaddress|awk -F ":" '{print $4""$5""$6 }')
 			set wireless.default_${name}.encryption=none
 EOF
 		uci -q commit wireless
